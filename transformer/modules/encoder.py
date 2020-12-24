712c2d74f715056fa5dc7c7367ea3ecbe49fe342
Ganesh Jagadeesan <ganesh.jcs@gmail.com>
2020-12-24 18:56:19 -0500

    added encoder type checks

--- transformer/modules/encoder.py
+++ transformer/modules/encoder.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from transformer.modules import clone_layer, Sublayer, LayerNorm
+from transformer.modules import clone_module, Sublayer, LayerNorm
 
 from typing import Union, List, Tuple
 
@@ -8,14 +8,14 @@ from typing import Union, List, Tuple
 class EncoderLayer(nn.Module):
     def __init__(self, size: Union[int, List[int], Tuple[int, ...]],
                  self_attn: nn.Module, feed_forward: nn.Module,
-                 dropout_prob: int):
+                 dropout_prob: int) -> None:
         super(EncoderLayer, self).__init__()
         self.self_attn = self_attn
         self.feed_forward = feed_forward
-        self.sublayer = clone_layer(Sublayer(size, dropout_prob), 2)
+        self.sublayer = clone_module(Sublayer(size, dropout_prob), 2)
         self.size = size
 
-    def forward(self, x: torch.Tensor, mask: torch.Tensor):
+    def forward(self, x: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
         x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
         return self.sublayer[1](x, self.feed_forward)
 
@@ -23,10 +23,10 @@ class EncoderLayer(nn.Module):
 class Encoder(nn.Module):
     def __init__(self, layer: EncoderLayer, N: int):
         super(Encoder, self).__init__()
-        self.layers = clone_layer(layer, N)
+        self.layers = clone_module(layer, N)
         self.norm = LayerNorm(layer.size)
 
-    def forward(self, x: torch.Tensor, mask: torch.Tensor):
+    def forward(self, x: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
         """
         Pass the input x(and mask) through each layer in turn.
         """
